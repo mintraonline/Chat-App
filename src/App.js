b1d851a538f98772237355f3ae5bb3e6fb4cf644
inshal <inshal.ansari@neosoftmail.com>
2025-09-01 11:13:40 +0530

    Path added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <AuthContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Chat-App">
         <Routes>
           <Route
             path="/"
